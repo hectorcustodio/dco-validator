5438268174ce22b34f26f27d9f798095d074647c
Hector Custódio <hectorcustodio@outlook.com.br>
2021-09-15 11:21:22 -0300

    Updates script

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+
+
+function validateDCOSignature(){
+  const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
+  const context = github.context
+
+  console.log('CONTEXT', context)
+  
+}
