0b4f9ba5a82c057497a2fab6823f768517514eaf
Hector Custódio <hectorcustodio@outlook.com.br>
2021-10-18 12:15:26 -0300

    Updates script
    Signed-off-by: Hector Custódio <hectorcustodio@outlook.com.br>

--- src/validator/index.js
+++ src/validator/index.js
@@ -19,8 +19,11 @@ const validateCommitSignatures = () => {
     const re = /(Signed-off-by:\s*)(.+)<(.+@.+)>/
 
     let step_A = commits.filter(({ author }) => (!authorsToSkip.split(',').includes(author.name)))
-    console.log("A", step_A.map((item)=> item.parents))
-    let step_B = step_A.filter(({ parents }) => (parents && !parents.length === 2))
+    console.log("A", step_A.map((item) => item.parents))
+    let step_B = step_A.filter(({ parents }) => {
+      console.log("PARENTS", parents, parents.length)
+      return (parents && !parents.length === 2)
+    })
     console.log("B", step_B)
     let step_C = step_B.map(({ author, message, sha }) => {
       const match = re.exec(message)
@@ -103,7 +106,7 @@ const validateCommitSignatures = () => {
     if (eventName === 'pull_request') {
       const { pull_request: pr } = payload
       const { data: prCommits } = await loadCommitsForPullRequest(pr.commits_url)
-      return prCommits.map(item => ({ ...item.commit, sha: item.sha, parents: [...item.parents] })) // github API return an object with the 'commit' key
+      return prCommits.map(item => ({ ...item.commit, sha: item.sha, parents: item.parents })) // github API return an object with the 'commit' key
     }
 
     if (eventName === 'push') {
